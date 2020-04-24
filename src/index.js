95454cb91b059d446b3a51a9ad2d3e4fd873c96e
Maksim <makstrofi@yandex.ru>
2020-04-24 16:54:11 +0300

    styles added

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
 import ErrorBoundry from './components/error-boundry';
 import BookstoreService from './services/bookstore-service';
-import { BookstoreServiceProvider } from './components/bookstore-service-context/';
+import { BookstoreServiceProvider } from './components/bookstore-service-context';
 
 import store from './store';
 
 const bookstoreService = new BookstoreService();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ErrorBoundry>
-            <BookstoreServiceProvider value={bookstoreService}>
-                <Router>
-                    <App />
-                </Router>
-            </BookstoreServiceProvider>
-        </ErrorBoundry>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <ErrorBoundry>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+          <App />
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoundry>
+  </Provider>,
+  document.getElementById('root')
 );
