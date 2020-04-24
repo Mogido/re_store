077bf8968826aa1db666266d58d80b3021954f5d
Maksim <makstrofi@yandex.ru>
2020-04-24 16:40:11 +0300

    learning to use action dispatch, compount function added

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item/';
+
 import { connect } from 'react-redux';
+import withBookstoreService from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils';
+
 import './book-list.css';
 
 class BookList extends Component {
+
+    componentDidMount() {
+        //receive data
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+        //dispatch action to store
+        this.props.booksLoaded(data);
+    }
 
     render() {
         const { books } = this.props;
@@ -25,4 +38,12 @@ const mapStateToProps = ({ books }) => {
     return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+//for details see https://www.udemy.com/course/pro-react-redux/learn/lecture/13258262#questions
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
