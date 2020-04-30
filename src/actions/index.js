3e03e2c7de674a375e93bba96c743d8e6ddf8b1a
Maksim <makstrofi@yandex.ru>
2020-04-30 20:25:44 +0300

    learning to use redux-thunk, some small refactoring in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -40,12 +40,19 @@ export const allBooksRemovedFromCart = (bookId) => {
   };
 };
 
-const fetchBooks = (bookstoreService, dispatch) => () => {
+const fetchBooksOld = (bookstoreService, dispatch) => () => {
   dispatch(booksRequested());
   bookstoreService.getBooks()
     .then((data) => dispatch(booksLoaded(data)))
     .catch((err) => dispatch(bookError(err)));
-}
+};
+
+const fetchBooks = (bookstoreService) => () => (dispatch) => {
+  dispatch(booksRequested());
+  bookstoreService.getBooks()
+    .then((data) => dispatch(booksLoaded(data)))
+    .catch((err) => dispatch(bookError(err)));
+};
 
 export {
   fetchBooks
