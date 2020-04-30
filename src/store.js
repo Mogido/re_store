d6659223901f9d5aa0c72e7c2f0eaab988878a32
Maksim <makstrofi@yandex.ru>
2020-04-30 18:57:44 +0300

    learning to use store enhancers

--- src/store.js
+++ src/store.js
@@ -2,6 +2,24 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            });
+        }
+
+        return originalDispatch(action);
+    };
+
+    return store;
+};
+
+const store = createStore(reducer, stringEnhancer);
 
 export default store;
